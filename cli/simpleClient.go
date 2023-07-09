bf4215cafaae9b4e1b68652854abc0543181a85f
donnie <donnie4w@gmail.com>
2023-07-09 16:29:14 +0800

    Update simpleClient.go

--- cli/simpleClient.go
+++ cli/simpleClient.go
@@ -21,6 +21,7 @@ type SimpleClient struct {
 	pingCount int
 	MqCli     *Cli
 	conf      *Config
+	zlib      bool
 	subMap    map[string]byte
 	//接收服务器数据
 	PullByteHandler func(mb *MqBean)
@@ -30,6 +31,8 @@ type SimpleClient struct {
 	PubMemHandler   func(jmb *JMqBean)
 	AckHandler      func(id int64)
 	ErrHandler      func(code int64)
+
+	Before func()
 }
 
 func (this *SimpleClient) Connect() (err error) {
@@ -39,7 +42,7 @@ func (this *SimpleClient) Connect() (err error) {
 	this.conf.Origin = this.Origin
 	this.conf.Auth = this.Auth
 	//出错后关闭连接并重新连接
-	this.conf.OnError = func(c *Cli, err error) {
+	this.conf.OnError = func(_ *Cli, _ error) {
 		logging.Error("OnError")
 		time.Sleep(1 * time.Second)
 		this.MqCli.Close()
@@ -47,7 +50,7 @@ func (this *SimpleClient) Connect() (err error) {
 	}
 
 	//处理服务器信息
-	this.conf.OnMessage = func(c *Cli, msg []byte) {
+	this.conf.OnMessage = func(_ *Cli, msg []byte) {
 		ty := msg[0]
 		if this.conf.RecvAckOn && (ty == MQ_PULLBYTE || ty == MQ_PULLJSON || ty == MQ_PUBJSON || ty == MQ_PUBBYTE || ty == MQ_MERGE) {
 			this.MqCli.ackMsg(msg)
@@ -63,10 +66,14 @@ func (this *SimpleClient) Connect() (err error) {
 		logging.Debug("reconn")
 		this.Connect()
 	}
+	<-time.After(time.Second)
 	if this.subMap != nil {
 		for k := range this.subMap {
 			this.Sub(k)
 		}
+	}
+	if this.Before != nil {
+		this.Before()
 	}
 	return
 }
@@ -97,20 +104,23 @@ func (this *SimpleClient) doMsg(msg []byte) {
 	case MQ_PING:
 		this.pingCount--
 	case MQ_MERGE:
-		if _r, err := ZlibUnCz(msg[1:]); err == nil {
-			logging.Info("merge >>", len(msg[1:]), " >> ", len(_r))
-			if mb, err := TDecode(_r, &MergeBean{}); err == nil {
+		var bs []byte
+		var err error
+		if this.zlib {
+			bs, err = ZlibUnCz(msg[1:])
+		} else {
+			bs = msg[1:]
+		}
+		if err == nil {
+			var mb *MergeBean
+			if mb, err = TDecode(bs, &MergeBean{}); err == nil {
 				for _, bl := range mb.BeanList {
 					this.doMsg(bl)
 				}
-				if len(mb.BeanList) == 0 {
-					logging.Error("len(mb.BeanList) == 0 ")
-				}
-			} else {
-				logging.Error("TDecode err >> ", err)
 			}
-		} else {
-			logging.Error("err >> ", err)
+		}
+		if err != nil {
+			logging.Error(err)
 		}
 	case MQ_ACK:
 		if r, err := BytesToInt64(msg[1:]); err == nil {
@@ -187,10 +197,21 @@ func (this *SimpleClient) PullJsonSync(topic string, id int64) (jmb *JMqBean, er
 	return this.MqCli.PullJsonSync(topic, id)
 }
 
+func (this *SimpleClient) PullIdSync(topic string) (id int64, err error) {
+	return this.MqCli.PullIdSync(topic)
+}
+
 func (this *SimpleClient) RecvAckOn(sec int8) (_r int64, err error) {
 	return this.MqCli.RecvAckOn(sec)
 }
 
 func (this *SimpleClient) MergeOn(size int8) (_r int64, err error) {
 	return this.MqCli.MergeOn(size)
+}
+
+func (this *SimpleClient) SetZlib(on bool) (_r int64, err error) {
+	if _r, err = this.MqCli.SetZlib(on); err == nil {
+		this.zlib = on
+	}
+	return
 }
